f53c3fd159dcaded15e07676e0ef6752c8eaf744
Filip <fpfpfp2002@gmail.com>
2024-03-06 16:11:28 +0100

    lab2 wo. the homework

--- lab2/zad2/distributed.py
+++ lab2/zad2/distributed.py
@@ -29,13 +29,41 @@ voteIter = nextVoteId()
 async def root() :
     return {"message" : "Hello World"}
 
-@app.post("/create_poll")
+@app.get("/poll")
+async def get_polls() :
+    return JSONResponse(status_code=status.HTTP_200_OK, content=[
+        {
+            "poll_id": i.id,
+            "possible_values": i.possible_values,
+        }
+        for i in polls
+    ])
+
+@app.post("/poll")
 async def create_poll(possibleValues: str):
     poll = Poll(next(pollIter), possibleValues.split('_'))
     polls.append(poll)
     return JSONResponse(status_code=status.HTTP_201_CREATED, content={"poll_id": poll.id, "possible_values": poll.possible_values})
 
-@app.put("/update_poll/{poll_id}")
+@app.get("/poll/{poll_id}")
+async def get_poll(poll_id:int) :
+    poll_index = None
+    for i in range(len(polls)):
+        if polls[i].id == poll_id:
+            poll_index = i
+            break
+
+    if poll_index == None:
+        return JSONResponse(status_code=status.HTTP_400_BAD_REQUEST, content=None)
+
+    return JSONResponse(status_code=status.HTTP_200_OK, content=
+    {
+        "poll_id": polls[poll_index].id,
+        "possible_values": polls[poll_index].possible_values
+    }
+    )
+
+@app.put("/poll/{poll_id}")
 async def update_poll(possibleValues: str, poll_id: int) :
     poll_index = None
     for i in range(len(polls)):
@@ -52,7 +80,7 @@ async def update_poll(possibleValues: str, poll_id: int) :
 
     return JSONResponse(status_code=status.HTTP_202_ACCEPTED, content={"poll_id": poll_id, "possible_values": possibleValues})
 
-@app.delete("/delete_poll/{poll_id}")
+@app.delete("/poll/{poll_id}")
 async def delete_poll(poll_id: int) :
     poll_index = None
     for i in range(len(polls)):
@@ -67,7 +95,7 @@ async def delete_poll(poll_id: int) :
 
     return JSONResponse(status_code=status.HTTP_200_OK, content=None)
 
-@app.post("/cast_vote/{poll_id}")
+@app.post("/poll/{poll_id}/vote")
 async def cast_vote(poll_id: int, value: str) :
     poll_index = None
     for i in range(len(polls)):
@@ -83,64 +111,7 @@ async def cast_vote(poll_id: int, value: str) :
         return JSONResponse(status_code=status.HTTP_400_BAD_REQUEST, content=None)
     return JSONResponse(status_code=status.HTTP_201_CREATED, content={"vote_id": vote.id, "vote_value": vote.value})
 
-@app.put("/update_vote/{poll_id}/{vote_id}")
-async def update_vote(poll_id: int, vote_id: int, value: str) :
-    poll_index = None
-    for i in range(len(polls)):
-        if polls[i].id == poll_id:
-            poll_index = i
-            break
-
-    if poll_index is None:
-        return JSONResponse(status_code=status.HTTP_400_BAD_REQUEST, content=None)
-
-    if polls[poll_index].updateVote(vote_id, value) == -1:
-        return JSONResponse(status_code=status.HTTP_400_BAD_REQUEST, content=None)
-
-    return JSONResponse(status_code=status.HTTP_201_CREATED, content={"vote_id": vote_id, "vote_value": value})
-
-@app.delete("/delete_vote/{poll_id}/{vote_id}")
-async def delete_vote(poll_id: int, vote_id: int) :
-    poll_index = None
-    for i in range(len(polls)):
-        if polls[i].id == poll_id:
-            poll_index = i
-            break
-
-    if polls[poll_index].removeVote(vote_id) == -1:
-        return JSONResponse(status_code=status.HTTP_400_BAD_REQUEST, content=None)
-
-    return JSONResponse(status_code=status.HTTP_200_OK, content=None)
-
-@app.get("/get_polls")
-async def get_polls() :
-    return JSONResponse(status_code=status.HTTP_200_OK, content=[
-        {
-            "poll_id": i.id,
-            "possible_values": i.possible_values,
-        }
-        for i in polls
-    ])
-
-@app.get("/get_options/{poll_id}")
-async def get_options(poll_id: int) :
-    poll_index = None
-    for i in range(len(polls)):
-        if polls[i].id == poll_id:
-            poll_index = i
-            break
-
-    if poll_index is None:
-        return JSONResponse(status_code=status.HTTP_400_BAD_REQUEST, content=None)
-
-    return JSONResponse(status_code=status.HTTP_200_OK, content=[
-        {
-            "value": i
-        }
-        for i in polls[poll_index].possible_values
-    ])
-
-@app.get("/get_votes/{poll_id}")
+@app.get("/poll/{poll_id}/vote")
 async def get_votes(poll_id: int) :
     poll_index = None
     for i in range(len(polls)):
@@ -159,7 +130,84 @@ async def get_votes(poll_id: int) :
         for i in polls[poll_index].votes
     ])
 
-@app.get("/get_results/{poll_id}")
+@app.get("/poll/{poll_id}/vote/{vote_id}")
+async def get_vote(poll_id: int, vote_id: int) :
+    poll_index = None
+    for i in range(len(polls)):
+        if polls[i].id == poll_id:
+            poll_index = i
+            break
+
+    if poll_index is None:
+        return JSONResponse(status_code=status.HTTP_400_BAD_REQUEST, content=None)
+
+    vote_index = None
+
+    for i in range(len(polls[poll_index].votes)):
+        if polls[poll_index].votes[i].id == vote_id:
+            vote_index = i
+            break
+
+    if vote_index is None:
+        return JSONResponse(status_code=status.HTTP_400_BAD_REQUEST, content=None)
+
+    return JSONResponse(status_code=status.HTTP_200_OK, content=
+        {
+            "vote_id": polls[poll_index].votes[vote_index].id,
+            "value": polls[poll_index].votes[vote_index].value,
+        }
+        )
+
+@app.put("/poll/{poll_id}/vote/{vote_id}")
+async def update_vote(poll_id: int, vote_id: int, value: str) :
+    poll_index = None
+    for i in range(len(polls)):
+        if polls[i].id == poll_id:
+            poll_index = i
+            break
+
+    if poll_index is None:
+        return JSONResponse(status_code=status.HTTP_400_BAD_REQUEST, content=None)
+
+    if polls[poll_index].updateVote(vote_id, value) == -1:
+        return JSONResponse(status_code=status.HTTP_400_BAD_REQUEST, content=None)
+
+    return JSONResponse(status_code=status.HTTP_201_CREATED, content={"vote_id": vote_id, "vote_value": value})
+
+@app.delete("/poll/{poll_id}/vote/{vote_id}")
+async def delete_vote(poll_id: int, vote_id: int) :
+    poll_index = None
+    for i in range(len(polls)):
+        if polls[i].id == poll_id:
+            poll_index = i
+            break
+
+    if polls[poll_index].removeVote(vote_id) == -1:
+        return JSONResponse(status_code=status.HTTP_400_BAD_REQUEST, content=None)
+
+    return JSONResponse(status_code=status.HTTP_200_OK, content=None)
+
+'''@app.get("/get_options/{poll_id}")
+async def get_options(poll_id: int) :
+    poll_index = None
+    for i in range(len(polls)):
+        if polls[i].id == poll_id:
+            poll_index = i
+            break
+
+    if poll_index is None:
+        return JSONResponse(status_code=status.HTTP_400_BAD_REQUEST, content=None)
+
+    return JSONResponse(status_code=status.HTTP_200_OK, content=[
+        {
+            "value": i
+        }
+        for i in polls[poll_index].possible_values
+    ])'''
+
+
+
+@app.get("/poll/{poll_id}/results")
 async def get_results(poll_id: int) :
     poll_index = None
     for i in range(len(polls)):
